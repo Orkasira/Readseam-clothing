aa75fd10bd92bda0a8eb815e47f11466604f3be0
Dachi Kekenadze <dachikekenadze5@gmail.com>
2025-09-20 23:07:15 +0400

    login page visual finished

--- src/LoginRegistration/Login/Login.jsx
+++ src/LoginRegistration/Login/Login.jsx
@@ -1,11 +1,55 @@
-import "Login.css"
+import "./Login.css";
+import photo from "../../assets/62bc5492a876268b6b9fc395f006a9259cafde47.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import eyeview from "../../assets/eyeview.png";
+import eyehide from "../../assets/eyehide.png";
 
 function Login() {
-    return (
-        <>
+  const [show, setShow] = useState(false);
 
-        </>
-    )
+  function togglePassword() {
+    setShow(!show);
+  }
+
+  return (
+    <>
+      <div className="login-container">
+        <div className="photo-container">
+          <img src={photo} alt="photo" className="photo" />
+        </div>
+        <form action="" className="form-container">
+          <h1 className="login-title">Log in</h1>
+
+          <div className="form-inputs-container">
+            <div className="input-container">
+              <input type="text" placeholder="Email or username *" />
+              <input
+                type={show ? "text" : "password"}
+                placeholder="Password *"
+              />
+              <span onClick={togglePassword}>
+                {show ? (
+                  <img src={eyeview} alt="eye" className="eye" />
+                ) : (
+                  <img src={eyehide} alt="eye" className="eye" />
+                )}
+              </span>
+            </div>
+
+            <button className="login-btn">Log in</button>
+
+            <p className="register-link-container">
+              Not a member?{" "}
+              <Link to="/Registration" className="register-link-content">
+                Register
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Login;
--- src/LoginRegistration/LoginRegistration.jsx
+++ src/LoginRegistration/LoginRegistration.jsx
@@ -1,11 +1,18 @@
 import Header from "./Header/Header.jsx";
+import Login from "./Login/Login.jsx";
+import Registration from "./Registration/Registration.jsx";
+import { Route, Routes } from "react-router";
 
 function LoginRegistration() {
-    return (
-        <>
-            <Header />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Registration" element={<Registration />} />
+      </Routes>
+    </>
+  );
 }
 
 export default LoginRegistration;
