1740b9f4addcdfb5879a4ab44a380f665d768bcf
Dachi Kekenadze <dachikekenadze5@gmail.com>
2025-09-25 00:13:58 +0400

    feat(ProductPage): added some products for 1 page

--- src/Products/ProductPage/ProductPage.jsx
+++ src/Products/ProductPage/ProductPage.jsx
@@ -1,9 +1,49 @@
 import "./ProductPage.css";
+import { useEffect, useState } from "react";
 
 function ProductPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const GetProduct = async () => {
+      try {
+        const res = await fetch(
+          "https://api.redseam.redberryinternship.ge/api/products",
+          {
+            method: "GET",
+            headers: {
+              accept: "application/json",
+            },
+          }
+        );
+        const result = await res.json();
+        setProducts(result.data);
+        console.log(result);
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    };
+
+    GetProduct();
+  }, []);
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <div className="product-page-container">
+        <div className="product-list">
+          {products.map((product) => (
+            <div key={product.id} className="product-card">
+              <img
+                src={product.cover_image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2 className="product-name">{product.name}</h2>
+              <p className="product-price">${product.price}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
